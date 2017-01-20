90fcf2c824d08301484c67b455715e2be9319d0b
Alan Souza <alansouzati@gmail.com>
2017-01-19 17:51:10 -0800

    v1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'grommet/scss/vanilla/index.scss';
+
+import App from './App';
+
+const element = document.getElementById('root');
+ReactDOM.render(<App />, element);
